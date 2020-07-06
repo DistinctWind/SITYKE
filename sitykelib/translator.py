6acebfc209fe2c07381e272d6c67e48c377b5071
DistinctWind <zq_zhouqi@qq.com>
2020-07-06 13:24:12 +0800

    修改程序引用(win32com)提高兼容性

--- sitykelib/translator.py
+++ sitykelib/translator.py
@@ -1,12 +1,12 @@
-import comtypes.client
+from win32com.client import Dispatch
 
 def init_powerpoint():
-    powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
+    powerpoint = Dispatch("Powerpoint.Application")
     return powerpoint
 
 def ppt2pdf(powerpoint, inputFileName, outputFileName):
     if not outputFileName.endswith(".pdf"):
         outputFileName = outputFileName + ".pdf"
     ppt = powerpoint.Presentations.Open(inputFileName)
-    ppt.ExportAsFixedFormat(outputFileName, 2)
+    ppt.ExportAsFixedFormat(outputFileName, 2, PrintRange=None)
     ppt.Close()
