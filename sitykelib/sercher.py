86d94eb576175eafa2c66b2f5d901025fb2413d4
DistinctWind <zq_zhouqi@qq.com>
2020-07-06 14:28:58 +0800

    添加doc2pdf功能

--- sitykelib/sercher.py
+++ sitykelib/sercher.py
@@ -10,3 +10,8 @@ def search_ppt(folder):
     files = os.listdir(folder)
     pptfiles = [f for f in files if f.endswith((".ppt", ".pptx"))]
     return pptfiles
+
+def search_doc(folder):
+    files = os.listdir(folder)
+    docfiles = [f for f in files if f.endswith((".doc", ".docx"))]
+    return docfiles
--- sitykelib/translator.py
+++ sitykelib/translator.py
@@ -4,9 +4,22 @@ def init_powerpoint():
     powerpoint = Dispatch("Powerpoint.Application")
     return powerpoint
 
+def init_word():
+    word = Dispatch("Word.Application")
+    return word
+
 def ppt2pdf(powerpoint, inputFileName, outputFileName):
     if not outputFileName.endswith(".pdf"):
         outputFileName = outputFileName + ".pdf"
     ppt = powerpoint.Presentations.Open(inputFileName)
     ppt.ExportAsFixedFormat(outputFileName, 2, PrintRange=None)
     ppt.Close()
+
+def doc2pdf(word, inputFileName, outputFileName):
+    if not outputFileName.endswith(".pdf"):
+        outputFileName = outputFileName + ".pdf"
+    doc = word.Documents.Open(inputFileName)
+    doc.ExportAsFixedFormat(outputFileName, 17)
+    # 传说，17是wdExportFormatPDF的值
+    # 那就这样用吧！
+    doc.Close()
