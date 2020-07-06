8a6c304e64f9eacc555e9f5d7337abc306ca45fd
DistinctWind <zq_zhouqi@qq.com>
2020-07-06 10:24:16 +0800

    提供程序入口

--- sitykelib/sercher.py
+++ sitykelib/sercher.py
@@ -1,6 +1,6 @@
 import os
 
-def search_ppt(powerpoint, folder):
+def search_ppt(folder):
     files = os.listdir()
     pptfiles = [f for f in files if f.endswith((".ppt", ".pptx"))]
     return pptfiles
