4625e2d5508c30a740a5bb688f916698dd63ca09
DistinctWind <zq_zhouqi@qq.com>
2020-07-06 11:09:01 +0800

    添加输出文件记录器

--- sitykelib/__init__.py
+++ sitykelib/__init__.py
@@ -4,11 +4,12 @@ import os
 cwd = os.getcwd()
 
 def main():
-    """
+    allFiles = []
     pptfiles = search_ppt(cwd)
     powerpoint = init_powerpoint()
     for pptfile in pptfiles:
         fullpath = os.path.join(cwd, pptfile)
+        allFiles.append(fullpath)
         ppt2pdf(powerpoint, fullpath, fullpath)
-    """
-    print(cwd)
+    
+    
