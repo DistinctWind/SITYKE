66d3ab74aa219bdaa5a78d737b6dda542621e665
DistinctWind <zq_zhouqi@qq.com>
2020-07-06 14:08:29 +0800

    添加一些提示消息

--- sitykelib/__init__.py
+++ sitykelib/__init__.py
@@ -21,4 +21,7 @@ def main():
     kindles = list_kindle()
     if len(kindles)==1:
         send_to_kindle(outputFiles, kindles[0])
-    
+    elif len(kindles)==0:
+        print("没有检测到kindle，复制取消")
+    else:
+        print("请自行复制或退出其他kindle再运行此程序")
--- sitykelib/kindle.py
+++ sitykelib/kindle.py
@@ -2,6 +2,8 @@ import os
 import string
 
 def list_disk():
+    # 盘符只可能是从A到Z
+    # 直接枚举吧
     disks = []
     for possibleDriveLetter in string.ascii_uppercase:
         possibleDrivePath = possibleDriveLetter+":\\"
@@ -10,13 +12,14 @@ def list_disk():
     return disks
 
 def judge_kindle(disk):
-    #貌似所有的kindle都会有这个文件夹呢
-    #话说连这个都没有的话那要怎么拷贝文件呢?
+    # 貌似所有的kindle都会有这个文件夹呢
+    # 话说连这个都没有的话那要怎么拷贝文件呢?
     allDir = os.listdir(disk)
     judgement = "documents"
     return judgement in allDir
 
 def list_kindle():
+    
     kindles = []
     disks = list_disk()
     for disk in disks:
