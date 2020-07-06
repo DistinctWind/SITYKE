75cec085168a0a4af27da362e1df0ccc3e8e4236
DistinctWind <zq_zhouqi@qq.com>
2020-07-06 10:24:27 +0800

    保存更改

--- sityke.py
+++ sityke.py
@@ -1,2 +1,11 @@
 from sitykelib.imports import *
+import os
 
+cwd = os.getcwd()
+
+if __name__ == "__main__":
+    pptfiles = search_ppt(cwd)
+    powerpoint = init_powerpoint()
+    for pptfile in pptfiles:
+        fullpath = os.path.join(cwd, pptfile)
+        ppt2pdf(powerpoint, fullpath, fullpath)
