a13a518d925d61d41d80a80a392e5221fff83b26
DistinctWind <zq_zhouqi@qq.com>
2020-07-06 10:07:45 +0800

    添加搜索ppt功能

--- sitykelib/translator.py
+++ sitykelib/translator.py
@@ -1,8 +1,8 @@
+import os
 import comtypes.client
 
 def init_powerpoint():
     powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
-    powerpoint.visable = 1
     return powerpoint
 
 def ppt2pdf(powerpoint, inputFileName, outputFileName):
@@ -10,3 +10,8 @@ def ppt2pdf(powerpoint, inputFileName, outputFileName):
         outputFileName = outputFileName + ".pdf"
     ppt = powerpoint.Presentations.Open(inputFileName)
     ppt.ExportAsFixedFormat(outputFileName, 2)
+
+def search_ppt(powerpoint, folder):
+    files = os.listdir()
+    pptfiles = [f for f in files if f.endswith((".ppt", ".pptx"))]
+    return pptfiles
