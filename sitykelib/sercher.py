89fda178a899af8469b2795af5e2321226d765d2
DistinctWind <zq_zhouqi@qq.com>
2020-07-08 12:15:32 +0800

    删除了画蛇添足地双引号

--- sitykelib/sercher.py
+++ sitykelib/sercher.py
@@ -8,10 +8,10 @@ def suffix(fileName):
 
 def search_ppt(folder):
     files = os.listdir(folder)
-    pptfiles = [f for f in files if f.endswith((".ppt", ".pptx"))]
+    pptfiles = [f for f in files if f.endswith((".ppt", ".pptx")) and not f.startswith("~$")]
     return pptfiles
 
 def search_doc(folder):
     files = os.listdir(folder)
-    docfiles = [f for f in files if f.endswith((".doc", ".docx"))]
+    docfiles = [f for f in files if f.endswith((".doc", ".docx")) and not f.startswith("~$")]
     return docfiles
