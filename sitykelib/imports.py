2d37035126b477787ccb1cc8243e021c54c7a4f2
DistinctWind <zq_zhouqi@qq.com>
2020-08-10 15:18:48 +0800

    添加users

--- sitykelib/imports.py
+++ sitykelib/imports.py
@@ -1,5 +1,6 @@
 from sitykelib.sercher import *
 from sitykelib.translator import *
 from sitykelib.kindle import *
+from sitykelib.users import *
 
 from tqdm import tqdm
---
+++ sitykelib/users.py
@@ -0,0 +1,9 @@
+def show_ppt_list(ppt_list):
+    print("检索演示文稿：")
+    for ppt_file_name in ppt_list:
+        print(ppt_file_name)
+
+def show_word_list(word_list):
+    print("检索文字文稿：")
+    for word_file_name in word_list:
+        print(word_file_name)
